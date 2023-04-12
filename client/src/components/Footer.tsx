574160f220e7f5e0cd391a29a4fb995f997b6eb8
Nathan <nslee333@gmail.com>
2023-04-11 21:26:30 -0700

    WIP: Header and Footer components.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,9 @@
 export default function Footer() {
   return (
     <>
+      <div className="absolute border h-32 w-32 text-white bottom-0 right-0">
+        <div className=" border">Footer</div>
+      </div>
     </>
-  )
+  );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,9 @@
 export default function Header() {
   return (
     <>
+      <div className="jusitify-center flex border text-white">
+        <div className="border">Header</div>
+      </div>
     </>
-  )
+  );
 }

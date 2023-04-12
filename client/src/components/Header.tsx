e9b8f875bdd36991121d6dd951422623694b00f0
Nathan <nslee333@gmail.com>
2023-04-12 15:34:55 -0700

    Header styles setup.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <>
-      <div className="jusitify-center flex border text-white">
-        <div className="border">Header</div>
+      <div className="jusitify-center flex border h-20 w-[100%] p-5 text-white">
+        <div className="flex justify-center h-18 w-[100%] border">Header</div>
       </div>
     </>
   );

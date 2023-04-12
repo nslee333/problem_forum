324132de98f2838d70144a51db54b14910ced8cd
Nathan <nslee333@gmail.com>
2023-04-12 15:35:35 -0700

    Prettier formatting changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <>
-      <div className="absolute border h-20 w-[100%] p-5 text-white bottom-0 right-0">
-        <div className="border flex justify-center h-18 w-[100%]">Footer</div>
+      <div className="absolute bottom-0 right-0 h-20 w-[100%] border p-5 text-white">
+        <div className="h-18 flex w-[100%] justify-center border">Footer</div>
       </div>
     </>
   );

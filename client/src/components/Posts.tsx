c4db73236fb3d46f7ccc447d1b7fe1ff09ee0a86
Nathan <nslee333@gmail.com>
2023-04-12 21:34:19 -0700

    Setup Posts to display multiple post entries.

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,13 +1,21 @@
 export default function Posts() {
+  const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  // const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+  const posts = array.map((number) => {
+    return (
+      <div
+        className="mx-1 my-2 flex h-[100%] w-auto justify-center overflow-scroll border text-white"
+        key={number}
+      >
+        Post #{number}
+      </div>
+    );
+  });
+
   return (
-    <div className='flex justiy-center border my-10'>
-    <div className='text-white border'>
-      Posts
-      
-    </div>
-    
-    
+    <div className="my-2 h-auto min-h-[74vh] w-[100%] overflow-scroll border">
+      <div>{posts}</div>
     </div>
   );
-  
 }

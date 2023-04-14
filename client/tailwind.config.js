315d68f39c94b26cd3a570700acc8d038c9a4fbe
Nathan <nslee333@gmail.com>
2023-04-14 14:47:20 -0700

    Added mobile screens to tailwind config.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+      screens: {
+        'ms': '320px',
+        'mm': '375px',
+        'ml': '425px',
+        
+        'xs': '320px',
+        'sm': '640px',
+        'md': '960px',
+        'lg': '1920px'
+      }
   },
   plugins: [],
 }

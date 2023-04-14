0b729b5ee2dd8c4825c81eec0438ef0abcc4f857
Nathan <nslee333@gmail.com>
2023-04-13 18:49:19 -0700

    Formatting for readability.

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,11 +1,25 @@
 export default function Posts() {
-  const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  // const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  const array: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+  ];
+  // const array: number[] = [
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 
+    // 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 
+    // 23, 24, 25, 26, 27, 28, 29, 30
+    // ];
 
   const posts = array.map((number) => {
     return (
       <div
-        className="mx-1 my-2 flex h-[100%] w-auto justify-center overflow-scroll border text-white"
+        className="
+                  mx-1 
+                  my-2 
+                  flex 
+                  h-[100%] 
+                  w-auto 
+                  justify-center 
+                  overflow-scroll 
+                  border text-white"
         key={number}
       >
         Post #{number}
@@ -14,7 +28,12 @@ export default function Posts() {
   });
 
   return (
-    <div className="my-2 h-auto min-h-[74vh] w-[100%] overflow-scroll border">
+    <div className="my-2 
+                    h-auto 
+                    min-h-[74vh] 
+                    w-[100%] 
+                    overflow-scroll 
+                    border">
       <div>{posts}</div>
     </div>
   );

c1bf633e8cf466037c5e0a2ba132fdf505aeb9c0
Nathan <nslee333@gmail.com>
2023-04-15 09:12:27 -0700

    Moved posts mock object and type to types file.

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,1 +1,86 @@
-export default {}
+export type returnObject = {
+  postId: number;
+  title: string;
+  upvoteCount: number;
+};
+
+export const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  // const array: number[] = [
+  // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  // 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  // 23, 24, 25, 26, 27, 28, 29, 30
+  // ];
+
+
+export const returnArr: returnObject[] = [
+  {
+    postId: 0,
+    title: "Alert system for AWS charges.",
+    upvoteCount: 132,
+  },
+  {
+    postId: 1,
+    title: "Alert system for AWS charges.",
+    upvoteCount: 120,
+  },
+  {
+    postId: 2,
+    title: "Alert system for AWS charges.",
+    upvoteCount: 100,
+  },
+  {
+    postId: 3,
+    title: "Alert system for AWS charges.",
+    upvoteCount: 84,
+  },
+  {
+    postId: 4,
+    title: "Alert system for AWS charges.",
+    upvoteCount: 32,
+  },
+  {
+    postId: 5,
+    title: "Alert system for AWS charges.",
+    upvoteCount: 3,
+  },
+  // {
+  //   postId: 2,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 100,
+  // },
+  // {
+  //   postId: 3,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 84,
+  // },
+  // {
+  //   postId: 4,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 32,
+  // },
+  // {
+  //   postId: 5,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 3,
+  // },
+  // {
+  //   postId: 2,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 100,
+  // },
+  // {
+  //   postId: 3,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 84,
+  // },
+  // {
+  //   postId: 4,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 32,
+  // },
+  // {
+  //   postId: 5,
+  //   title: "Alert system for AWS charges.",
+  //   upvoteCount: 3,
+  // },
+];

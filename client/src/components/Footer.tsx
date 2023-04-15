19e421e91a366689bc84906f375675de671502fb
Nathan <nslee333@gmail.com>
2023-04-14 19:04:18 -0700

    Styling for Footer.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,31 @@
 export default function Footer() {
   return (
     <>
-      <div className="h-30 mt-2 flex justify-center border p-5 text-white">
-        <div className="h-18 flex w-[100%] justify-center border">Footer</div>
+      <div
+        className="
+                    h-30 
+                    ms:m
+                    flex
+                    p-5
+                    text-white 
+                    ms:mb-10
+                    ms:mt-4 
+                    xs:justify-center
+                    "
+      >
+        <div
+          className="
+                      h-18 
+                      flex 
+                      justify-center
+                      ms:w-[90%]
+                      xs:text-sm
+                      sm:w-2/3
+                      sm:text-xl
+                      "
+        >
+          Connecting Problems with Problem-Solvers.
+        </div>
       </div>
     </>
   );

60885d3a007382ab0bc675f9f013393654ceb861
Nathan <nslee333@gmail.com>
2023-04-17 22:19:05 -0700

    Added Body to TSX.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Posts from "./components/Posts";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="min-h-[100vh] ">
       <Header />
-      <Posts />
+      <Body />
       <Footer />
     </div>
   );

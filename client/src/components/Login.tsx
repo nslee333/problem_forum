3903c48dfebfeac87b58a0ae0419a9314c3cc455
Nathan <nslee333@gmail.com>
2023-04-18 10:54:41 -0700

    Added styling to Login.

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,8 +1,34 @@
 export default function Login() {
   return (
     <>
-      <div>
-        Login
+      <div
+        className="
+                  flex 
+                  justify-center 
+                  "
+      >
+        <div
+          className="
+                    my-2
+                    h-auto
+                    w-[48rem] 
+                    overflow-auto 
+                    rounded-md 
+                    bg-[#3A3A3A] 
+                    xs:min-h-[75vh]
+                    xs:p-2
+                    sm:min-h-[80vh]
+                    sm:pl-3
+                    sm:pt-3
+                    
+                    "
+        >
+          <div className="bg-[#3A3A3A] text-white">
+          Login
+           
+           
+          </div>
+        </div>
       </div>
     </>
   );

64dc0459bc4bfbabb6b5cb353f9e98aaae18cda9
Nathan <nslee333@gmail.com>
2023-04-18 10:52:35 -0700

    Removed Body.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Posts from "./components/Posts";
-import Body from "./components/Body";
+import Login from "./components/Login";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="min-h-[100vh] ">
       <Header />
-      <Body>
-        
-      </Body>
+        <About />
       <Footer />
     </div>
   );

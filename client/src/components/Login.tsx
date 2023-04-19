91d31974287b791411debf59429178546a9bd762
Nathan <nslee333@gmail.com>
2023-04-18 17:50:33 -0700

    Setup forms for Login.

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,7 +3,7 @@ export default function Login() {
     <>
       <div
         className="
-                  flex 
+                  flex
                   justify-center 
                   "
       >
@@ -23,10 +23,56 @@ export default function Login() {
                     
                     "
         >
-          <div className="bg-[#3A3A3A] text-white">
-          Login
-           
-           
+          <div
+            className="   m-5
+                          p-5
+                          border 
+                          bg-[#3A3A3A] 
+                          text-white"
+          >
+            <form className="bg-[#3A3A3A]">
+              <label className="bg-[#4A4A4A] mt-1">Email</label>
+              <input className="bg-[#4A4A4A] mt-1 " type="email" placeholder="..." />
+
+              <label className="bg-[#4A4A4A] mt-1">Password</label>
+              <input
+                className="bg-[#4A4A4A] mt-1"
+                type="password"
+                placeholder="..."
+              />
+
+              <button className="bg-[#4A4A4A] p-0.5">Sign In</button>
+            </form>
+          </div>
+          
+          <div className="bg-[#3A3A3A] text-white border">
+            <label className="bg-[#4A4A4A] mt-1">Username</label>
+            <input className="bg-[#4A4A4A] mt-1 " type="text" placeholder="..." />
+
+            <label className="bg-[#4A4A4A] mt-1">Email</label>
+            <input
+              className="bg-[#4A4A4A] mt-1"
+              type="email"
+              placeholder="..."
+            />
+
+            <label className="bg-[#4A4A4A] mt-1">Password</label>
+            <input
+              className="bg-[#4A4A4A] mt-1"
+              type="email"
+              placeholder="..."
+            />
+            <div className='bg-[#3a3a3a]'>Strength indicator here</div>
+
+            <label className="bg-[#4A4A4A] mt-1"> Confirm Password</label>
+            <input
+              className="bg-[#4A4A4A] mt-1"
+              type="email"
+              placeholder="..."
+            />
+
+            <button>Sign Up</button>
+
           </div>
         </div>
       </div>
